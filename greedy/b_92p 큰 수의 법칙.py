62342bac2a44bb86bd97ac9879f1d0ff4e3c754c
kiv9908 <leebongwook0805@gmail.com>
2024-03-23 18:52:43 +0900

    큰 수의 법칙

--- greedy/b_92p 큰 수의 법칙.py
+++ greedy/b_92p 큰 수의 법칙.py
@@ -1,2 +1,27 @@
-a = 0
-b = 1
+# 배열의 크기 n
+# 숫자가 더해지는 횟수 m
+# 연속 더하기 가능 횟수 k
+
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort(reverse=True)
+count = 0
+answer = 0
+
+# 가장 큰 수 data[0]
+
+# 두번째로 큰 수 data[1]
+
+while (count!=m):
+    for i in range(k):
+        if count == m:
+            break
+        answer += data[0]
+        count+=1
+    
+    answer += data[1]
+    count+=1
+
+print(answer)
