0cc0ac29301b88d67e8bc27a3c5b6a95fcdc3bc2
kiv9908 <leebongwook0805@gmail.com>
2024-03-24 16:56:30 +0900

    조이스틱 sol!

--- programmers/b_조이스틱.py
+++ programmers/b_조이스틱.py
@@ -14,11 +14,13 @@ def solution(name):
         next = i + 1
         while next < len(name) and name[next] == 'A':
             next += 1
-            
+        print("i : ",i," , ",name[i]," next : ", next," , ", [min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
         # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
 				# for문 돌면서 왼쪽으로 갈지 오른쪽으로 갈지 매번 체크를 한다.
         min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
-        
+
     # 알파벳 변경(상하이동) 횟수에 좌우이동 횟수 추가
+    print(answer," + ", min_move)
     answer += min_move
+    
     return answer
