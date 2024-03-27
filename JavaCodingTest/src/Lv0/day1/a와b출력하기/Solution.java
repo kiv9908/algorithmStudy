0fbb3f00d5981a683b8895912eb764936cb7b492
User <User@DESKTOP-0EU21N8>
2024-03-27 12:00:14 +0900

    240327 프로그래머스 lv.0 시작

---
+++ JavaCodingTest/src/Lv0/day1/a와b출력하기/Solution.java
@@ -0,0 +1,20 @@
+package Lv0.day1.a와b출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+//		String str = sc.nextLine();
+//
+//		int a = Integer.parseInt(str.split(" ")[0]);
+//		int b = Integer.parseInt(str.split(" ")[1]);
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+}
