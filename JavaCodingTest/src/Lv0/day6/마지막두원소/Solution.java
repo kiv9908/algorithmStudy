edb3d00707a4d331bb618de81ab8324538536681
kevinlee <leebongwook0805@gmail.com>
2024-04-01 13:25:22 +0900

    240401 test2

--- JavaCodingTest/src/Lv0/day6/마지막두원소/Solution.java
+++ JavaCodingTest/src/Lv0/day6/마지막두원소/Solution.java
@@ -2,8 +2,23 @@ package Lv0.day6.마지막두원소;
 
 class Solution {
 	public int[] solution(int[] num_list) {
-		int[] answer = {};
+		int[] answer = new int[num_list.length + 1];
+
+		for (int i = 0; i < num_list.length; i++) {
+			answer[i] = num_list[i];
+
+		}
+
+		int last = num_list[num_list.length - 1];
+		int lastt = num_list[num_list.length - 2];
+
+		if (last > lastt) {
+			answer[answer.length - 1] = last - lastt;
+		} else {
+			answer[answer.length - 1] = last * 2;
+		}
 
 		return answer;
 	}
+
 }
