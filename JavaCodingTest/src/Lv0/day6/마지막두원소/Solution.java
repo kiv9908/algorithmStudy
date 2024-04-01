ca91eff4ba41da054f9b7f20ce44476a83808b54
kevinlee <leebongwook0805@gmail.com>
2024-04-01 11:58:14 +0900

    Revert "240401 test"
    This reverts commit 27387ec57c659ea52b74760c5bcebc2e1567f2a1.

--- JavaCodingTest/src/Lv0/day6/마지막두원소/Solution.java
+++ JavaCodingTest/src/Lv0/day6/마지막두원소/Solution.java
@@ -2,24 +2,8 @@ package Lv0.day6.마지막두원소;
 
 class Solution {
 	public int[] solution(int[] num_list) {
-		// 배열 선언 [ ] 안에 배열 개수 쓰기
-		int[] answer = new int[num_list.length + 1];
-
-		for (int i = 0; i < num_list.length; i++) {
-			answer[i] = num_list[i];
-
-		}
-		// 배열의 맨 마지막 인덱스는 길이 -1 (인덱스가 0부터 시작하기 때문)
-		int last = num_list[num_list.length - 1];
-		int lastt = num_list[num_list.length - 2];
-
-		if (last > lastt) {
-			answer[answer.length - 1] = last - lastt;
-		} else {
-			answer[answer.length - 1] = last * 2;
-		}
+		int[] answer = {};
 
 		return answer;
 	}
-
 }
