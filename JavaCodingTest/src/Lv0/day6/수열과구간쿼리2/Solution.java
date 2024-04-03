4edca1c0b2b2cf9af5ee5c8e7fbb4a9b045a18d4
kiv9908 <leebongwook0805@gmail.com>
2024-04-03 23:24:13 +0900

    240403

--- JavaCodingTest/src/Lv0/day6/수열과구간쿼리2/Solution.java
+++ JavaCodingTest/src/Lv0/day6/수열과구간쿼리2/Solution.java
@@ -1,21 +1,4 @@
 package Lv0.day6.수열과구간쿼리2;
-/*
- * 
-import java.util.stream.IntStream;
-
-class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return IntStream.range(0, queries.length)
-                .map(q -> IntStream.rangeClosed(queries[q][0], queries[q][1])
-                        .map(i -> arr[i])
-                        .filter(i -> i > queries[q][2])
-                        .min().orElse(-1)
-                ).toArray();
-    }
-}
-
- */
 
 class Solution {
 	public static int[] solution(int[] arr, int[][] queries) {
