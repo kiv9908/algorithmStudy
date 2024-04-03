ad41163d896ef0c12e48c488f3e5b8e28a0bf332
kiv9908 <leebongwook0805@gmail.com>
2024-04-03 23:17:55 +0900

    240403 lv.0 6day

---
+++ JavaCodingTest/src/Lv0/day6/수열과구간쿼리2/Solution.java
@@ -0,0 +1,49 @@
+package Lv0.day6.수열과구간쿼리2;
+/*
+ * 
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        return IntStream.range(0, queries.length)
+                .map(q -> IntStream.rangeClosed(queries[q][0], queries[q][1])
+                        .map(i -> arr[i])
+                        .filter(i -> i > queries[q][2])
+                        .min().orElse(-1)
+                ).toArray();
+    }
+}
+
+ */
+
+class Solution {
+	public static int[] solution(int[] arr, int[][] queries) {
+		int[] answer = new int[queries.length];
+
+		for (int i = 0; i < queries.length; i++) {
+			int s = queries[i][0];
+			int e = queries[i][1];
+			int k = queries[i][2];
+			boolean flag = false;
+
+			for (int j = s; j < e + 1; j++) {
+				if (arr[j] > k) {
+					flag = true;
+					if (answer[i] == 0) {
+						answer[i] = arr[j];
+					} else {
+						if (answer[i] > arr[j]) {
+							answer[i] = arr[j];
+						}
+					}
+
+				}
+			}
+			if (!flag) {
+				answer[i] = -1;
+			}
+		}
+		return answer;
+	}
+}
--- JavaCodingTest/src/Lv0/day6/수열과구간쿼리3/Solution.java
+++ JavaCodingTest/src/Lv0/day6/수열과구간쿼리3/Solution.java
@@ -1,21 +1,18 @@
 package Lv0.day6.수열과구간쿼리3;
 
 class Solution {
-
-	public static void main(String[] arg) {
-		int[] arr = { 0, 1, 2, 3, 4 };
-		int[][] queries = { { 0, 4, 2 }, { 0, 3, 2 }, { 0, 2, 2 } };
-		int[] answer = new int[queries.length];
+	public int[] solution(int[] arr, int[][] queries) {
+//		int[] answer = new int[arr.length];
 
 		for (int i = 0; i < queries.length; i++) {
-			int s = queries[i][0];
-			int e = queries[i][1];
-			int k = queries[i][2];
 
-			for (int j = 0; j < queries[i].length; j++) {
+			int x = arr[queries[i][0]];
+			int y = arr[queries[i][1]];
 
-			}
+			arr[queries[i][0]] = y;
+			arr[queries[i][1]] = x;
 		}
 
+		return arr;
 	}
 }
