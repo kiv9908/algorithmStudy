75874539b4643bc281143ec92da7e6320bbb529e
kiv9908 <leebongwook0805@gmail.com>
2024-04-13 14:28:51 +0900

    240413 9로 나눈 나머지

--- JavaCodingTest/src/programmers/Lv_0/day8/구로나눈나머지/Solution.java
+++ JavaCodingTest/src/programmers/Lv_0/day8/구로나눈나머지/Solution.java
@@ -3,7 +3,13 @@ package programmers.Lv_0.day8.구로나눈나머지;
 class Solution {
 	public int solution(String number) {
 		int answer = 0;
+		int sum = 0;
 
-		return answer;
+		for (int i = 0; i < number.length(); i++) {
+
+			answer += Integer.parseInt(String.valueOf(number.charAt(i)));
+
+		}
+		return answer %= 9;
 	}
 }
