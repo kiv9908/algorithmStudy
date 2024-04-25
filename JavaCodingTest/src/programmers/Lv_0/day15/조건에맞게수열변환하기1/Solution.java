3a40b564b04c3d7642d90bed2fabc55098b29425
kevinlee <leebongwook0805@gmail.com>
2024-04-25 17:09:37 +0900

    240425 lv.0 day15 조건에맞게수열변환하기2

--- JavaCodingTest/src/programmers/Lv_0/day15/조건에맞게수열변환하기1/Solution.java
+++ JavaCodingTest/src/programmers/Lv_0/day15/조건에맞게수열변환하기1/Solution.java
@@ -1,16 +1,32 @@
 package programmers.Lv_0.day15.조건에맞게수열변환하기1;
 
-class Solution {
-	public int[] solution(int[] arr) {
+import java.util.Arrays;
 
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] >= 50 && arr[i] % 2 == 0) {
-				arr[i] /= 2;
-			} else if (arr[i] < 50 && arr[i] % 2 != 0) {
-				arr[i] *= 2;
+class Solution {
+	public int solution(int[] arr) {
+
+		int[] arrCopy = Arrays.copyOf(arr, arr.length);
+		int count = 0;
+
+		while (true) {
+
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] >= 50 && arr[i] % 2 == 0) {
+					arr[i] /= 2;
+				} else if (arr[i] < 50 && arr[i] % 2 != 0) {
+					arr[i] = arr[i] * 2 + 1;
+				}
 			}
+
+			if (Arrays.equals(arr, arrCopy)) {
+				break;
+			} else {
+				arrCopy = Arrays.copyOf(arr, arr.length);
+				count++;
+			}
+
 		}
 
-		return arr;
+		return count;
 	}
 }
