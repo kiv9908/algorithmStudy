a95575fe4d4ad8f439c505fdf983864a15fbb000
kiv9908 <leebongwook0805@gmail.com>
2024-04-26 23:41:16 +0900

    0426 lv.0 day15 1로만들기

---
+++ JavaCodingTest/src/programmers/Lv_0/day15/일로만들기/Solution.java
@@ -0,0 +1,20 @@
+package programmers.Lv_0.day15.일로만들기;
+
+class Solution {
+	public int solution(int[] num_list) {
+		int answer = 0;
+
+		for (int i = 0; i < num_list.length; i++) {
+
+			while (num_list[i] != 1) {
+
+				num_list[i] /= 2;
+				answer++;
+
+			}
+
+		}
+
+		return answer;
+	}
+}
