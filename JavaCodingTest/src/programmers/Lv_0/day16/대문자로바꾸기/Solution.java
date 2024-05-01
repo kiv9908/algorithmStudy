69376421c92fbbc45191c3c0af3cb2264c02c137
kiv9908 <leebongwook0805@gmail.com>
2024-05-01 09:51:20 +0900

    Merge branch 'main' of https://github.com/kiv9908/algorithmStudy into main

---
+++ JavaCodingTest/src/programmers/Lv_0/day16/대문자로바꾸기/Solution.java
@@ -0,0 +1,11 @@
+package programmers.Lv_0.day16.대문자로바꾸기;
+
+class Solution {
+	public String solution(String myString) {
+		String answer = "";
+
+		answer = myString.toUpperCase();
+
+		return answer;
+	}
+}
---
+++ JavaCodingTest/src/programmers/Lv_0/day16/소문자로바꾸기/Solution.java
@@ -0,0 +1,11 @@
+package programmers.Lv_0.day16.소문자로바꾸기;
+
+class Solution {
+	public String solution(String myString) {
+		String answer = "";
+
+		answer = myString.toLowerCase();
+
+		return answer;
+	}
+}
