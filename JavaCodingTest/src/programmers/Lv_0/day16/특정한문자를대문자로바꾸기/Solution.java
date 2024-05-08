b5c83e13fcb10123c6261ba150bfb146dbffcd37
bongwook <leebongwook0805@gmail.com>
2024-05-08 21:05:44 +0900

    240508 lv.0 day16 특정한문자를대문자로바꾸기

--- JavaCodingTest/src/programmers/Lv_0/day16/특정한문자를대문자로바꾸기/Solution.java
+++ JavaCodingTest/src/programmers/Lv_0/day16/특정한문자를대문자로바꾸기/Solution.java
@@ -1,8 +1,17 @@
 package programmers.Lv_0.day16.특정한문자를대문자로바꾸기;
 
 class Solution {
-    public String solution(String my_string, String alp) {
-        String answer = "";
-        return answer;
-    }
+	public String solution(String my_string, String alp) {
+		String answer = "";
+
+		for (int i = 0; i < my_string.length(); i++) {
+			if (alp.equals(my_string.charAt(i) + "")) {
+				answer += (my_string.charAt(i) + "").toUpperCase();
+			} else {
+				answer += my_string.charAt(i);
+			}
+		}
+
+		return answer;
+	}
 }
