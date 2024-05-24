da67c27e467b6a6d69db5dc4252c20faba1767cc
bongwook <leebongwook0805@gmail.com>
2024-05-24 22:58:05 +0900

    240524

--- JavaCodingTest/src/programmers/Lv_0/day17/공백으로구분하기2/Solution.java
+++ JavaCodingTest/src/programmers/Lv_0/day17/공백으로구분하기2/Solution.java
@@ -3,7 +3,7 @@ package programmers.Lv_0.day17.공백으로구분하기2;
 class Solution {
 	public String[] solution(String my_string) {
 		String[] answer = {};
-
+		// 정규식 표현 사용하기
 		answer = my_string.trim().split("\\s+");
 
 		return answer;
