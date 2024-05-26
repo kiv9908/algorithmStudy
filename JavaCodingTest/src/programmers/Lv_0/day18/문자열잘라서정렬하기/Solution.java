494b91516f75a9a651199b2fada7bfa02f031e73
bongwook <leebongwook0805@gmail.com>
2024-05-27 00:08:32 +0900

    0527

--- JavaCodingTest/src/programmers/Lv_0/day18/문자열잘라서정렬하기/Solution.java
+++ JavaCodingTest/src/programmers/Lv_0/day18/문자열잘라서정렬하기/Solution.java
@@ -1,9 +1,9 @@
 package programmers.Lv_0.day18.문자열잘라서정렬하기;
 
+import java.util.Arrays;
+
 class Solution {
 	public String[] solution(String myString) {
-		String[] answer = myString.split("x");
-
-		return answer;
+		return Arrays.stream(myString.split("x")).filter(s -> !s.isEmpty()).sorted().toArray(String[]::new);
 	}
 }
